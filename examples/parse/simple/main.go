61fc7a9441abf2fc93f6f8b7ca0e5bf128305695
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-20 10:44:59 +0330

    add: simple example

--- examples/parse/simple/main.go
+++ examples/parse/simple/main.go
@@ -1,1 +1,30 @@
-package simple
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/amirhnajafiz/explorer"
+)
+
+type Object struct {
+	Name  string `json:"name"`
+	Value int    `json:"value"`
+}
+
+func main() {
+	obj := &Object{
+		Name:  "amir",
+		Value: 20,
+	}
+
+	bytes, _ := json.Marshal(obj)
+
+	objMap, err := explorer.ParseJsonObject(bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(objMap)
+}
+
