43cf9416b0c00682098ca5a0f54d91d51b5ce3dd
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-20 10:52:15 +0330

    add: array parsing test

---
+++ test/json_array_test.go
@@ -0,0 +1,39 @@
+package test
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/amirhnajafiz/explorer"
+	"testing"
+)
+
+// TestArrayParsing.
+func TestArrayParsing(t *testing.T) {
+	collection := []obj{
+		{
+			Name:  "peter",
+			Value: 45,
+		},
+		{
+			Name:  "amirhossein",
+			Value: 20,
+		},
+		{
+			Name:  "daniel",
+			Value: 100,
+		},
+	}
+
+	bytes, _ := json.Marshal(collection)
+
+	// check parsing
+	objMap, err := explorer.ParseJsonArray(bytes)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// check array
+	if len(objMap) != 3 {
+		t.Error(errors.New("data loss while parsing"))
+	}
+}
