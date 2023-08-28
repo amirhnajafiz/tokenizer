ca4bf3b55941bf7b492d4a8e49e11162fc30faed
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-28 09:46:46 +0330

    add: main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// todo: Add cobra
+	// todo: Add set token
+	// todo: Add view tokens
+	// todo: Remove token
+	// todo: Update token
+	// todo: Add get token
+	// todo: Add encryption
+	// todo: Feat encrypt option
+}
