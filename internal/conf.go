246fe2c40a7408a08b481e316d49f3872a6ffa4a
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-28 19:21:46 +0330

    feat: check file

--- internal/conf.go
+++ internal/conf.go
@@ -1,8 +1,21 @@
 package internal
 
+import "os"
+
+const baseFile = "conf.txt"
+
 // CheckFile returns true if conf file exists
 func CheckFile() bool {
-	return true
+	_, err := os.Stat(baseFile)
+	if err == nil {
+		return true
+	}
+
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	return false
 }
 
 // InitFile creates conf file
