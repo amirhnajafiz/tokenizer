f295d616788c27e56968143e9bf25f8c10e77074
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-28 19:17:42 +0330

    add: crypto key

--- internal/encrypt.go
+++ internal/encrypt.go
@@ -1,11 +1,17 @@
 package internal
 
+import "github.com/amirhnajafiz/encrypto"
+
+const (
+	private = "xx#40op3^i)jn2jf"
+)
+
 // Code converts a cipher into coded value
 func Code(cipher string) (string, error) {
-	return "", nil
+	return encrypto.Encrypt(private, cipher)
 }
 
 // DeCode converts a coded value into cipher
 func DeCode(token string) (string, error) {
-	return "", nil
+	return encrypto.Decrypt(private, token)
 }
