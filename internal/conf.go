cfe63f1527b6fb6a77eb8cd22b7d38857778b92b
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-28 10:54:55 +0330

    init: conf

---
+++ internal/conf.go
@@ -0,0 +1,11 @@
+package internal
+
+// CheckFile returns true if conf file exists
+func CheckFile() bool {
+	return true
+}
+
+// InitFile creates conf file
+func InitFile() error {
+	return nil
+}
