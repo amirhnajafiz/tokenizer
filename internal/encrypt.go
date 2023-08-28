e40e0c50fbb90ccc02580ff406289bb6ac04f043
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-28 10:50:50 +0330

    init: methods

--- internal/encrypt.go
+++ internal/encrypt.go
@@ -1,9 +1,11 @@
 package internal
 
+// Code converts a cipher into coded value
 func Code(cipher string) (string, error) {
 	return "", nil
 }
 
+// DeCode converts a coded value into cipher
 func DeCode(token string) (string, error) {
 	return "", nil
 }
--- internal/io.go
+++ internal/io.go
@@ -1,11 +1,11 @@
 package internal
 
-// Set new value.
+// Set new value
 func Set(key, value string) error {
 	return nil
 }
 
-// Get existing value.
+// Get existing value
 func Get(key string) (string, error) {
 	return "", nil
 }
