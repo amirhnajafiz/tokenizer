f445ae3509aa51832c7e2c4250422a557c2b2d2e
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-29 10:14:09 +0330

    add: encrypt key

--- internal/encrypt.go
+++ internal/encrypt.go
@@ -1,17 +1,31 @@
 package internal
 
-import "github.com/amirhnajafiz/encrypto"
+import (
+	"os"
+
+	"github.com/amirhnajafiz/encrypto"
+)
 
 const (
 	private = "xx#40op3^i)jn2jf"
 )
 
+// get encryption private key
+func getKey() string {
+	key := os.Getenv("TK_PRIVATE")
+	if len(key) == 0 {
+		key = private
+	}
+
+	return key
+}
+
 // Code converts a cipher into coded value
 func Code(cipher string) (string, error) {
-	return encrypto.Encrypt(private, cipher)
+	return encrypto.Encrypt(getKey(), cipher)
 }
 
 // DeCode converts a coded value into cipher
 func DeCode(token string) (string, error) {
-	return encrypto.Decrypt(private, token)
+	return encrypto.Decrypt(getKey(), token)
 }
