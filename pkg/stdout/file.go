148a0548dbf8509e9c61901f2f5f56b7db3cb1a0
amirhnajafiz <najafizadeh21@gmail.com>
2024-07-09 17:44:34 +0300

    remove: old internal file handler

--- pkg/stdout/file.go
+++ pkg/stdout/file.go
@@ -1,1 +1,21 @@
 package stdout
+
+import (
+	"fmt"
+	"os"
+)
+
+func (s Stdout) exportToFile(path string, data ...string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %v", err)
+	}
+
+	for _, item := range data {
+		if _, err := file.Write([]byte(item)); err != nil {
+			return fmt.Errorf("failed to write content: %v", err)
+		}
+	}
+
+	return nil
+}
