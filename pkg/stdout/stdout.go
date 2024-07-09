1ebb4b0019547630d6b6cf81ca1b5339b27a8155
amirhnajafiz <najafizadeh21@gmail.com>
2024-07-09 17:39:29 +0300

    add: stdout options

--- pkg/stdout/stdout.go
+++ pkg/stdout/stdout.go
@@ -2,7 +2,11 @@ package stdout
 
 // Stdout is a struct that is used for displaying
 // the application output in multiple interfaces.
-type Stdout struct{}
+type Stdout struct {
+	Clipboard bool
+	File      bool
+	Path      string
+}
 
 func (s Stdout) Print(input ...string) error {
 	return nil
